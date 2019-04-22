ac270f256b326fd1ef801ccce158d6da86c4440d
BiYuqi <biyuqiwan@163.com>
2019-04-22 20:39:21 +0800

    fix error

--- src/components/Usage.js
+++ src/components/Usage.js
@@ -6,13 +6,11 @@ import './Usage.scss';
 
 const passDynamicParam = (inputValue) => {
   return `
-    \`\`\`shell
     # xyz:数字类型的权限属性，为rwx属性数值的相加
     # —R:进行递归，可修改子目录下的文件
     chmod [-R] xyz 文件名/目录名
     # 修改test.txt的权限为777
     chmod ${inputValue} test.txt
-    \`\`\`
   `;
 };
 
