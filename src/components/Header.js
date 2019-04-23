460f74616aa846e74ae68e5c1dde9c65609368f0
BiYuqi <biyuqiwan@163.com>
2019-04-23 18:14:13 +0800

    add author

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.scss'
 const Header = () => {
   return (
     <div className="chmod-header">
-      <h1>Chmod Calculator</h1>
+      <h1>Chmod Calculator<a href="https://github.com/BiYuqi/chmod-calculator">@BiYuqi</a></h1>
       <p>A fancy calculator convert Linux file permissions between different format </p>
     </div>
   );
